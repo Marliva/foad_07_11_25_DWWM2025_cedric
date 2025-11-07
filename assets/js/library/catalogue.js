838eecb0dee293645046290d34fdf12761c472e2
Marliva <cedriclevasseur76@gmail.com>
2025-11-07 15:57:41 +0100

    Rental car App

---
+++ assets/js/library/catalogue.js
@@ -0,0 +1,40 @@
+const catalogue = [
+    {
+        modele : "Peugeot 208",
+        carburant : "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000km (0.5/km supplémentaire),",
+        prix : 999,
+        cover : "vehicule1.png",
+    },
+    {
+        modele : "Ford Focus",
+        carburant : "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000km (0.5/km supplémentaire),",
+        prix : 999,
+        cover : "vehicule2.png",
+    },
+    {
+        modele : "Audi A1",
+        carburant : "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000km (0.5/km supplémentaire),",
+        prix : 1100,
+        cover : "vehicule3.jpg",
+    },
+    {
+        modele : "Opel Mokka",
+        carburant : "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000km (0.5/km supplémentaire),",
+        prix : 1150,
+        cover : "vehicule4.png",
+    },
+    {
+        modele : "DS7",
+        carburant : "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000km (0.5/km supplémentaire),",
+        prix : 1300,
+        cover : "ds7.jpg",
+    },
+    {
+        modele : "Hyundai i30",
+        carburant : "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000km (0.5/km supplémentaire),",
+        prix : 800,
+        cover : "i30.jpg",
+    },
+];
+
+export { catalogue }
---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+import { hero } from "./components/hero.js";
+import { burger } from "./components/menu.js";
+
+
