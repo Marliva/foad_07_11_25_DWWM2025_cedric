714ad66d784623869bc3062ecc6363cb395c8b02
Marliva <cedriclevasseur76@gmail.com>
2025-11-07 18:50:58 +0100

    Responsive + slider cards

--- assets/js/components/cards.js
+++ assets/js/components/cards.js
@@ -6,10 +6,11 @@ catalogue.forEach((car) => {
   result.textContent = `${catalogue.length} résultats`;
   const card = document.createElement("div");
   card.classList.add("card");
+  let index = 0;
   card.innerHTML = `
         <div>
             <i class="fa-solid fa-caret-left"></i>
-            <img src="./assets/img/${car.cover}" alt="${car.modele}" />
+            <img src="./assets/img/${car.cover[index]}" alt="${car.modele}" />
             <i class="fa-solid fa-caret-right"></i>
         </div>
         <div>
@@ -19,7 +20,20 @@ catalogue.forEach((car) => {
             <button type="button">Réserver et Payer</button>
         </div>
     `;
-  cars.appendChild(card);
+  cars.append(card);
+
+  const img = card.querySelector("img");
+  const previous = card.querySelector(".fa-caret-left");
+  const next = card.querySelector(".fa-caret-right");
+
+  next.addEventListener("click", () => {
+      index = (index + 1) % car.cover.length;
+      img.src = `./assets/img/${car.cover[index]}`;
+    });
+    previous.addEventListener("click", () => {
+      index = (index - 1 + car.cover.length) % car.cover.length;
+      img.src = `./assets/img/${car.cover[index]}`;
+    });
 });
 
 export { cars };
--- assets/js/library/catalogue.js
+++ assets/js/library/catalogue.js
@@ -3,37 +3,37 @@ export const catalogue = [
         modele : "Peugeot 208",
         carburant : "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000km (0.5/km supplémentaire),",
         prix : 999,
-        cover : "vehicule1.png",
+        cover : ["vehicule1.png", "208interieur.jpg"],
     },
     {
         modele : "Ford Focus",
         carburant : "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000km (0.5/km supplémentaire),",
         prix : 999,
-        cover : "vehicule2.png",
+        cover : ["vehicule2.png", "focusinterieur.jpg"],
     },
     {
         modele : "Audi A1",
         carburant : "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000km (0.5/km supplémentaire),",
         prix : 1100,
-        cover : "vehicule3.webp",
+        cover : ["vehicule3.webp", "a1interieur.jpg"],
     },
     {
         modele : "Opel Mokka",
         carburant : "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000km (0.5/km supplémentaire),",
         prix : 1150,
-        cover : "vehicule4.png",
+        cover : ["vehicule4.png", "mokkainterieur.jpg"],
     },
     {
         modele : "DS7",
         carburant : "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000km (0.5/km supplémentaire),",
         prix : 1300,
-        cover : "ds7.jpg",
+        cover : ["ds7.jpg", "ds7interieur.jpg"],
     },
     {
         modele : "Hyundai i30",
         carburant : "Diesel, 5 portes, GPS, AutoRadio, Forfait 1000km (0.5/km supplémentaire),",
         prix : 800,
-        cover : "i30.jpg",
+        cover : ["i30.jpg", "i30interieur.jpg"],
     },
 ];
 
